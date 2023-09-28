b4c4784f1acf68295f9d8c377e5fd752d06d4a50
Andrii Gadar <gadar.andre@gmail.com>
2023-09-28 12:01:40 +0300

    server settings

--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const sendEmail = async (date) => {
-  const email = { ...date, from: "sanjaksms@gmail.com" };
+  const email = { ...date, from: "userandrii@meta.ua" };
   try {
     await sgMail.send(email);
     return true;
